1f7ab4ee49823aed3fa73169c504b3a29cecefee
Eslam <eslam.ismail93@gmail.com>
2020-09-10 01:46:45 +0200

    first commit

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use Illuminate\Http\Request;
+use Redis;
+use Storage;
+
+class ChatController extends Controller
+{
+    public function getMessages() //get messages
+
+    {
+        $messages = Message::all();
+
+        return response()->json($messages, 200);
+    }
+    public function sendMessage(Request $request) //send messages
+
+    {
+        $data = $request->only(['name', 'body']);
+        if ($request->has('image')) {
+            $data['image'] = Storage::disk('uploads')->put('chat', $request->image);
+        }
+        $message = Message::create($data);
+
+        $redis = Redis::connection();
+        $redis->publish('message', json_encode($message));
+
+        return response()->json($message, 200);
+    }
+}
